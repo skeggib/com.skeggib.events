892b69eefd9d85770dcb461d8d128c4b29127a4c
Sebastien Klasa <klasa.sebastien@gmail.com>
2017-05-22 16:04:17 +0200

    v0.1

--- src/com/skeggib/events/EventArgs.java
+++
@@ -1,5 +0,0 @@
-package com.skeggib.events;
-
-public class EventArgs {
-
-}
--- src/com/skeggib/events/IEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.skeggib.events;
-
-public interface IEventHandler<Targs> {
-
-	public void handle(Object sender, Targs args);
-
-}
